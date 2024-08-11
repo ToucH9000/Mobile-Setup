90c4f886f29cc507f4b10e85b8015d04ff5e637b
اُسامه وحىد <85633117+ToucH9000@users.noreply.github.com>
2024-08-11 06:46:05 +0500

    Add files via upload

--- Files/susfs4ksu/service.sh
+++ Files/susfs4ksu/service.sh
@@ -13,3 +13,5 @@ ${SUSFS_BIN} add_sus_path /data/adbroot
 ${SUSFS_BIN} add_sus_path /vendor/bin/install-recovery.sh
 
 ${SUSFS_BIN} add_sus_path /system/etc/hosts
+
+${SUSFS_BIN} set_uname 'default' 'default' '4.14.180-perf-g950783ac4623' 'default' 'default'
