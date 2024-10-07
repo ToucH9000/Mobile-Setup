af5a49b2f95cdf1a77da669cfc7420d99543e55b
اُسامه وحىد <85633117+ToucH9000@users.noreply.github.com>
2024-10-07 06:56:09 +0500

    Update boot-completed.sh

--- Files/susfs4ksu/boot-completed.sh
+++ Files/susfs4ksu/boot-completed.sh
@@ -10,3 +10,6 @@ ${SUSFS_BIN} add_sus_path /system/addon.d
 ${SUSFS_BIN} add_sus_path /system/etc/hosts
 
 ${SUSFS_BIN} add_sus_path /vendor/bin/install-recovery.sh
+
+# Remove "#" in line 15 to set your kernel name to default "perf"
+#${SUSFS_BIN} set_uname '4.14.180-perf-g950783ac4623' 'default'
