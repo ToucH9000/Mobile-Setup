cc30d6c838e19173fb9ed8e5c2ddb34bfdbb9983
اسامه وحيد <85633117+ToucH9000@users.noreply.github.com>
2024-11-17 08:33:23 +0500

    Add files via upload

--- Files/boot-completed.sh
+++ Files/boot-completed.sh
@@ -10,18 +10,21 @@ ${SUSFS_BIN} add_sus_path /system/addon.d
 ${SUSFS_BIN} add_sus_path /system/etc/hosts
 ${SUSFS_BIN} add_sus_path /vendor/bin/install-recovery.sh
 
-resetprop -n ro.boot.vbmeta.size 9728
-resetprop -n ro.boot.vbmeta.hash_alg sha256
-resetprop -n ro.fota.oem Xiaomi
-resetprop -n ro.opa.eligible_device true
 resetprop -n ro.boot.flash.locked 1
-resetprop -n ro.secureboot.devicelock 1
-resetprop -n ro.secureboot.lockstate locked
-resetprop -n ro.com.google.acsa true
-resetprop -n ro.com.google.gmsversion 12_202207
+resetprop -n ro.boot.mode normal
+resetprop -n ro.boot.selinux enforcing
 resetprop -n ro.boot.vbmeta.device_state locked
 resetprop -n ro.boot.vbmeta.digest ac088f34ae6c95568a9e95ac63821dee4f334cf94537409dd360556d781776de
+resetprop -n ro.boot.vbmeta.hash_alg sha256
+resetprop -n ro.boot.vbmeta.size 9728
 resetprop -n ro.boot.veritymode enforcing
 resetprop -n ro.boot.veritymode.managed yes
-resetprop -n ro.boot.mode normal
 resetprop -n ro.bootmode normal
+resetprop -n ro.build.selinux 1
+resetprop -n ro.build.selinux.enforce 1
+resetprop -n ro.com.google.acsa true
+resetprop -n ro.com.google.gmsversion 12_202207
+resetprop -n ro.fota.oem Xiaomi
+resetprop -n ro.opa.eligible_device true
+resetprop -n ro.secureboot.devicelock 1
+resetprop -n ro.secureboot.lockstate locked
